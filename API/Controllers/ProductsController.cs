9fc523a2c3918065af7306e97c32f301b964608d
Nikola Hadzhiev <nikihad2012@abv.bg>
2023-09-24 18:36:44 +0300

    Fixed GetProduct method to return NotFound when wrong Product id is given

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -24,7 +24,11 @@ namespace API.Controllers
         [HttpGet("{id}")]
         public async Task<ActionResult<Product>> GetProduct(int id)
         {
-            return await _context.Products.FindAsync(id);
+            var product = await _context.Products.FindAsync(id);
+            
+            if(product == null) return NotFound();
+
+            return product;
         }
     }
 }
